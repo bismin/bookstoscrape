d24d9789e40abe1a61a8f20161c75653ed007df2
reshma <reshu@datahut.co>
2018-09-17 17:45:50 +0530

    adding file

--- book.py
+++ book.py
@@ -9,12 +9,12 @@ class BookSpider(scrapy.Spider):
 
     def parse(self, response):
     	category=response.xpath('//div[@class="page-header action"]/h1/text()').extract_first()
-    	jobs=response.xpath('//li[@class="col-xs-6 col-sm-4 col-md-3 col-lg-3"]/article[@class="product_pod"]')
+    	jobs=response.xpath('//li/article')
     	for job in jobs:
     			title=job.xpath('h3/a/text()').extract_first('')
     			rating=job.xpath('p/@class').extract_first('')
-    			price=job.xpath('div[@class="product_price"]/p[@class="price_color"]/text()').extract_first('')
-    			stock=job.xpath('div[@class="product_price"]/p[@class="instock availability"]/text()').extract()
+    			price=job.xpath('div/p/text()').extract_first('')
+    			stock=job.xpath('div/p[@class="instock availability"]/text()').extract()
     			rating = rating.replace('star-rating ','')
     			stock = stock[1].strip() if stock else ''
     			yield{'title':title,'rating':rating,'price':price,'stock':stock,'category':category}
